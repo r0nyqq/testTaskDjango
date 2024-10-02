75d63356dc034328431ec10fc249e529d30962f7
r0nyqq <r0nyqkurin@gmail.com>
2024-10-03 03:44:02 +0400

    Revert "test changes"
    This reverts commit 9a0b1eb5f5e7f8a77b0a7385f37a0bc9dbde05d3.

--- app_news/news_board/urls.py
+++ app_news/news_board/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import NewsListView, NewsDetailView
 
 urlpatterns = [
-    path('news_list1/', NewsListView.as_view(), name='news_list'),
-    path('news_list/<int:p1k>/', NewsDetailView.as_view(), name='detail_news')
+    path('news_list/', NewsListView.as_view(), name='news_list'),
+    path('news_list/<int:pk>/', NewsDetailView.as_view(), name='detail_news')
 ]
